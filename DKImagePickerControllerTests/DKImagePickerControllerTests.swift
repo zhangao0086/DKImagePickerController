ad8c8cce155ba8935bf78a50cb1323d3b4668feb
ryochan1101 <ryo1101@mercari.com>
2016-02-12 13:01:57 +0900

    new framework created

---
+++ DKImagePickerControllerTests/DKImagePickerControllerTests.swift
@@ -0,0 +1,36 @@
+//
+//  DKImagePickerControllerTests.swift
+//  DKImagePickerControllerTests
+//
+//  Created by mercari on 2/12/16.
+//  Copyright © 2016 ZhangAo. All rights reserved.
+//
+
+import XCTest
+@testable import DKImagePickerController
+
+class DKImagePickerControllerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
