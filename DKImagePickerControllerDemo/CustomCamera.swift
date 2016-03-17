a7117347e9331e39042088b3ff4f22621287bbfa
Bannings <zhangao0086@gmail.com>
2016-03-17 16:29:23 +0800

    Added CustomUIDelegate.

---
+++ DKImagePickerControllerDemo/CustomCamera.swift
@@ -0,0 +1,24 @@
+//
+//  CustomCamera.swift
+//  DKImagePickerControllerDemo
+//
+//  Created by ZhangAo on 16/3/17.
+//  Copyright © 2016年 ZhangAo. All rights reserved.
+//
+
+import UIKit
+
+public class CustomUIDelegate: DKImagePickerControllerUIDelegate {
+	
+	@objc public func imagePickerControllerCreateCamera(imagePickerController: DKImagePickerController, didCancel: (() -> Void), didFinishCapturingImage: ((image: UIImage) -> Void)) -> UIViewController {
+		let vc = UIViewController()
+		vc.view.backgroundColor = UIColor.redColor()
+		
+		let delayTime = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
+		dispatch_after(delayTime, dispatch_get_main_queue()) {
+			didCancel()
+		}
+		
+		return vc
+	}
+}
