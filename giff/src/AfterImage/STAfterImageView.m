e5e61d3273da20240ab649f5781d4529ca3e5108
metasmile <cyrano905@gmail.com>
2016-07-11 18:15:54 +0900

    refactor STAfterImageView

--- giff/src/AfterImage/STAfterImageView.m
+++ giff/src/AfterImage/STAfterImageView.m
@@ -23,8 +23,7 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        _afterImageSublayersContainerView = [[UIView alloc] initWithSize:self.size];
-        [self insertSubview:_afterImageSublayersContainerView aboveSubview:_contentView];
+
     }
     return self;
 }
@@ -37,23 +36,21 @@
 - (void)setImageSet:(STCapturedImageSet *)imageSet {
     NSAssert(!imageSet || [imageSet.extensionObject isKindOfClass:[STAfterImageItem class]], @"given imageSet did not contain STAfterImageItem in .extensionData");
 
+    BOOL newImageSet = ![imageSet isEqual:_imageSet];
+    _imageSet = imageSet;
+
     //sub set
-    if(imageSet) {
+    if(_imageSet) {
         //set - heavy cost
-        if (![imageSet isEqual:_imageSet] && [imageSet.extensionObject isKindOfClass:[STAfterImageItem class]]) {
-            STCapturedImage * anyImage = [imageSet.images firstObject];
+        if (newImageSet) {
+            STCapturedImage * anyImage = [_imageSet.images firstObject];
             NSAssert(anyImage.imageUrl, @"STCapturedImage's imageUrl does not exist.");
-            NSArray<NSURL *>* imageUrls = [imageSet.images mapWithItemsKeyPath:@keypath(anyImage.fullScreenUrl) orDefaultKeypath:@keypath(anyImage.imageUrl)];
+            NSArray<NSURL *>* imageUrls = [_imageSet.images mapWithItemsKeyPath:@keypath(anyImage.fullScreenUrl) orDefaultKeypath:@keypath(anyImage.imageUrl)];
 
-            _afterImageItem = (STAfterImageItem *)imageSet.extensionObject;
-
-            for (STAfterImageLayerItem *layerItem in _afterImageItem.layers) {
-                STSelectableView *layerView = [[STSelectableView alloc] initWithSize:_afterImageSublayersContainerView.size];
-                layerView.fitViewsImageToBounds = YES;
-
-                //TODO: preheating - 여기서 미리 랜더링된 필터를 temp url에 저장 후 그 url을 보여주는 것도 나쁘지 않을듯
-                [_afterImageSublayersContainerView addSubview:layerView];
-                [layerView setViews:imageUrls];
+            if([_imageSet.extensionObject isKindOfClass:[STAfterImageItem class]]){
+                _afterImageItem = (STAfterImageItem *)_imageSet.extensionObject;
+            }else{
+                _afterImageItem = nil;
             }
 
             [self setViews:imageUrls];
@@ -63,23 +60,56 @@
         _afterImageItem = nil;
         [self clearViews];
     }
+}
 
-    _imageSet = imageSet;
+- (void)setAfterImageLayerViews:(NSArray<NSURL *>*)imageUrls{
+    if(!_afterImageItem){
+        return;
+    }
+
+    if(!_afterImageSublayersContainerView){
+        _afterImageSublayersContainerView = [[UIView alloc] initWithSize:self.size];
+        [self insertSubview:_afterImageSublayersContainerView aboveSubview:_contentView];
+    }
+
+    for (STAfterImageLayerItem *layerItem in _afterImageItem.layers) {
+        STSelectableView *layerView = [[STSelectableView alloc] initWithSize:_afterImageSublayersContainerView.size];
+        layerView.fitViewsImageToBounds = YES;
+
+        //TODO: preheating - 여기서 미리 랜더링된 필터를 temp url에 저장 후 그 url을 보여주는 것도 나쁘지 않을듯
+        [_afterImageSublayersContainerView addSubview:layerView];
+        [layerView setViews:imageUrls];
+    }
+}
+
+- (void)clearAfterImageLayerViews{
+    if(_afterImageItem){
+       return;
+    }
+
+    [_afterImageSublayersContainerView st_eachSubviews:^(UIView *view, NSUInteger index) {
+        [((STSelectableView *) view) clearViews];
+    }];
+    [_afterImageSublayersContainerView clearAllOwnedImagesIfNeeded:NO removeSubViews:YES];
+}
+
+#pragma mark From STSelectableView
+- (void)setViews:(NSArray *)presentableObjects {
+    [self setAfterImageLayerViews:presentableObjects];
+    [super setViews:presentableObjects];
 }
 
 - (void)clearViews {
-    if(!_afterImageItem){
-        [_afterImageSublayersContainerView st_eachSubviews:^(UIView *view, NSUInteger index) {
-            [((STSelectableView *) view) clearViews];
-        }];
-        [_afterImageSublayersContainerView clearAllOwnedImagesIfNeeded:NO removeSubViews:YES];
-    }
+    [self clearAfterImageLayerViews];
     [super clearViews];
 }
 
 - (void)setViewsDisplay {
     [super setViewsDisplay];
 
+    if(!CGSizeEqualToSize(_afterImageSublayersContainerView.size, self.size)){
+        _afterImageSublayersContainerView.size = self.size;
+    }
     [_afterImageSublayersContainerView.subviews eachViewsWithIndex:^(UIView *view, NSUInteger index) {
         STSelectableView * layerView = (STSelectableView *) view;
         STAfterImageLayerItem *layerItem = [_afterImageItem.layers st_objectOrNilAtIndex:index];
@@ -96,6 +126,5 @@
         }
     }];
 }
-
 
 @end
