6feb62e4368b11890f432b806fe861dadbc60326
metasmile <cyrano905@gmail.com>
2016-07-21 17:37:08 +0900

    clear

--- giff/src/AnimatableLayer/effects/STGIFFDisplayLayerJanneEffect.m
+++ giff/src/AnimatableLayer/effects/STGIFFDisplayLayerJanneEffect.m
@@ -80,46 +80,6 @@
                 ,composeItem1
 //                ,composeItem2
         ] processForImage:YES] imageFromCurrentFramebuffer];
-
-//        GPUImagePicture * sourcePicture = [[GPUImagePicture alloc] initWithImage:sourceImages[0] smoothlyScaleOutput:YES];
-//
-//        GPUImagePicture * overlayPicture = [[GPUImagePicture alloc] initWithImage:[UIImage imageBundled:@"input.jpg"] smoothlyScaleOutput:YES];
-//
-//        GPUImageLightenBlendFilter *blendFilter = [[GPUImageLightenBlendFilter alloc] init];
-//
-//        //color
-//        GPUImageRGBFilter * colorFilter_add = [[GPUImageRGBFilter alloc] init];
-//        NSArray* colors_add = [UIColorFromRGB(0xff0000) rgbaArray];
-//        colorFilter_add.red = 1;
-//        colorFilter_add.green = 0;
-//        colorFilter_add.blue = 0;
-//
-////    [colorFilter_add setColorRed:[colors_add[0] floatValue]
-////                           green:[colors_add[1] floatValue]
-////                            blue:[colors_add[2] floatValue]];
-//
-//        GPUImageSobelEdgeDetectionFilter * edgeDetectionFilter = [[GPUImageSobelEdgeDetectionFilter alloc] init];
-//        edgeDetectionFilter.edgeStrength = 4;
-//
-//        [overlayPicture addTarget:edgeDetectionFilter];
-//        [edgeDetectionFilter addTarget:colorFilter_add];
-//        [colorFilter_add addTarget:blendFilter];
-//
-//        GPUImageBrightnessFilter * brightnessFilter = [[GPUImageBrightnessFilter alloc] init];
-//        brightnessFilter.brightness = -.5f;
-//
-//        [sourcePicture addTarget:brightnessFilter];
-//        [brightnessFilter addTarget:blendFilter];
-//
-//        [blendFilter useNextFrameForImageCapture];
-//
-//        [overlayPicture useNextFrameForImageCapture];
-//        [overlayPicture processImage];
-//
-//        [sourcePicture useNextFrameForImageCapture];
-//        [sourcePicture processImage];
-//
-//        return [blendFilter imageFromCurrentFramebuffer];
     }
 }
 
