ca5c20ab18c9a5cbca5149df9a3d3f05845b0854
Bannings <zhangao0086@gmail.com>
2016-08-06 19:37:22 +0800

    If a camera is not available, don't pops-up "Max photos limit reached".

--- DKImagePickerController/View/DKAssetGroupDetailVC.swift
+++ DKImagePickerController/View/DKAssetGroupDetailVC.swift
@@ -329,11 +329,13 @@ internal class DKAssetGroupDetailVC: UIViewController, UICollectionViewDelegate,
 		cell.setCameraImage(self.imagePickerController.UIDelegate.imagePickerControllerCameraImage())
         
         cell.didCameraButtonClicked = { [unowned self] in
-            if UIImagePickerController.isSourceTypeAvailable(.Camera) && self.imagePickerController.selectedAssets.count < self.imagePickerController.maxSelectableCount  {
-                self.imagePickerController.presentCamera()
-			} else {
-				self.imagePickerController.UIDelegate.imagePickerControllerDidReachMaxLimit(self.imagePickerController)
-			}
+            if UIImagePickerController.isSourceTypeAvailable(.Camera) {
+                if self.imagePickerController.selectedAssets.count < self.imagePickerController.maxSelectableCount  {
+                    self.imagePickerController.presentCamera()
+                } else {
+                    self.imagePickerController.UIDelegate.imagePickerControllerDidReachMaxLimit(self.imagePickerController)
+                }
+            }
         }
 
         return cell
