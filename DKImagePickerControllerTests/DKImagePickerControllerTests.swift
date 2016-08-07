fc537990a36a96972e43a34ebf9b31e24dd06a35
Bannings <zhangao0086@gmail.com>
2016-08-07 17:28:40 +0800

    Trying to fix Travis CI.

--- DKImagePickerControllerTests/DKImagePickerControllerTests.swift
+++ DKImagePickerControllerTests/DKImagePickerControllerTests.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2016年 ZhangAo. All rights reserved.
 //
 
+import UIKit
 import XCTest
-@testable import DKImagePickerController
 
 class DKImagePickerControllerTests: XCTestCase {
     
@@ -23,12 +23,12 @@ class DKImagePickerControllerTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssert(true, "Pass")
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure() {
             // Put the code you want to measure the time of here.
         }
     }
