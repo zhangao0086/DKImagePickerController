7f3d2966ffb7adf9e4a74bee9309da49d3903846
metasmile <cyrano905@gmail.com>
2016-08-10 12:54:23 +0900

    eb - add play action

--- giff/src-giffe/EBRootViewController.m
+++ giff/src-giffe/EBRootViewController.m
@@ -14,6 +14,7 @@
 #import "STGIFFDisplayLayerEffectItem.h"
 #import "NSString+STUtil.h"
 #import "STGIFFDisplayLayerColorizeEffect.h"
+#import "BlocksKit.h"
 
 @implementation EBRootViewController {
 
@@ -37,8 +38,19 @@
         layerSet.layers = [layerSet.layers arrayByAddingObject:[STCapturedImageSetAnimatableLayer layerWithImageSet:images[1]]];
         [[STGIFFDisplayLayerEffectsManager sharedManager] prepareLayerEffectFrom:images[1] forLayerSet:layerSet];
     }
-
     [_layerSetPresentationView appendLayerSet:layerSet];
+
+
+    //play animation
+    if(_layerSetPresentationView.currentLayerSet.frameCount>1){
+        [NSTimer bk_scheduledTimerWithTimeInterval:.1 block:^(NSTimer *timer) {
+            if(_layerSetPresentationView.currentFrameIndex>=_layerSetPresentationView.currentLayerSet.frameCount-1){
+                _layerSetPresentationView.currentFrameIndex=0;
+            }else{
+                _layerSetPresentationView.currentFrameIndex++;
+            }
+        } repeats:YES];
+    }
 }
 
 @end
