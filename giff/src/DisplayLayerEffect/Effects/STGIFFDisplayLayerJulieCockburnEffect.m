f220ced987bd1255bb82ec9f0d9b72a570b49d33
metasmile <cyrano905@gmail.com>
2016-08-21 15:14:21 +0900

    add blenders for STGIFFDisplayLayerJulieCockburnEffect

--- giff/src/DisplayLayerEffect/Effects/STGIFFDisplayLayerJulieCockburnEffect.m
+++ giff/src/DisplayLayerEffect/Effects/STGIFFDisplayLayerJulieCockburnEffect.m
@@ -12,11 +12,49 @@
 #import "GPUImageMaskFilter.h"
 #import "GPUImageTransformFilter.h"
 #import "GPUImageTransformFilter+STGPUImageFilter.h"
+#import "NSNumber+STUtil.h"
+#import "NSObject+BNRTimeBlock.h"
+#import "GPUImageDarkenBlendFilter.h"
+#import "GPUImageNormalBlendFilter.h"
+#import "GPUImageLightenBlendFilter.h"
+#import "GPUImageSoftLightBlendFilter.h"
+#import "GPUImageDifferenceBlendFilter.h"
+#import "GPUImageSubtractBlendFilter.h"
+#import "GPUImageAddBlendFilter.h"
+#import "GPUImageColorBlendFilter.h"
+#import "GPUImageExclusionBlendFilter.h"
+#import "GPUImageLuminosityBlendFilter.h"
+#import "GPUImageMultiplyBlendFilter.h"
+#import "GPUImageSourceOverBlendFilter.h"
+#import "GPUImageLinearBurnBlendFilter.h"
+#import "GPUImageColorDodgeBlendFilter.h"
+#import "GPUImageScreenBlendFilter.h"
+#import "GPUImageOverlayBlendFilter.h"
+#import "GPUImageDissolveBlendFilter.h"
+#import "NSArray+STUtil.h"
 
 
 @implementation STGIFFDisplayLayerJulieCockburnEffect {
-
+    NSArray * const BlendingFiltersClassNames;
 }
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        BlendingFiltersClassNames = @[
+                NSStringFromClass(GPUImageNormalBlendFilter.class),
+                NSStringFromClass(GPUImageSourceOverBlendFilter.class),
+                NSStringFromClass(GPUImageSoftLightBlendFilter.class),
+                NSStringFromClass(GPUImageAddBlendFilter.class),
+                NSStringFromClass(GPUImageLightenBlendFilter.class),
+                NSStringFromClass(GPUImageMultiplyBlendFilter.class),
+                NSStringFromClass(GPUImageScreenBlendFilter.class)
+        ];
+    }
+
+    return self;
+}
+
 
 - (NSArray *)composersToProcess:(NSArray<UIImage *> *__nullable)sourceImages {
 
@@ -29,19 +67,47 @@
 
     NSMutableArray * composers = NSMutableArray.array;
 
+    //get cliped image
     [composers addObject:[[STGPUImageOutputComposeItem itemWithSourceImage:maskImage composer:GPUImageMaskFilter.new] addFilters:@[
-            [GPUImageTransformFilter transform:CGAffineTransformMakeRotation(AGKDegreesToRadians(30))]
+//            [GPUImageTransformFilter transform:CGAffineTransformMakeRotation(AGKDegreesToRadians(30))]
     ]]];
     [composers addObject:[[STGPUImageOutputComposeItem itemWithSourceImage:sourceImages[0]] addFilters:@[
-            [GPUImageTransformFilter transform:CGAffineTransformMakeRotation(AGKDegreesToRadians(30))]
+//            [GPUImageTransformFilter transform:CGAffineTransformMakeRotation(AGKDegreesToRadians(30))]
     ]]];
+    UIImage * clipedImage = [self processComposers:[composers reverse]];
 
+    [composers removeAllObjects];
+
+    CGFloat gapOfDegree = 36;
+
+    //GPUImageNormalBlendFilter -> original
+    //GPUImageSourceOverBlendFilter -> original
+    //GPUImageSoftLightBlendFilter
+    //GPUImageAddBlendFilter
+    //GPUImageLightenBlendFilter
+    //GPUImageMultiplyBlendFilter
+    //GPUImageScreenBlendFilter
+
+    //GPUImageDissolveBlendFilter -> normal but removed original pixels.
+
+    //GPUImageDifferenceBlendFilter -> clipped but awesome.
+    
+    NSUInteger count = (NSUInteger) ((180+gapOfDegree) / gapOfDegree);
+    for(id index in [@(count) st_intArray]){
+        @autoreleasepool {
+            CGFloat degree = gapOfDegree * [index floatValue];
+//            NSString * blenderClassName = [blendingFiltersClassNames st_objectOrNilAtIndex:[index unsignedIntegerValue]];
+            NSString * blenderClassName = [BlendingFiltersClassNames st_objectOrNilAtIndex:randomir(1,BlendingFiltersClassNames.count-1)];
+            GPUImageTwoInputFilter * blender = ([index unsignedIntegerValue]==count-1) ?
+                    nil : (GPUImageTwoInputFilter *)[NSClassFromString(blenderClassName ?: BlendingFiltersClassNames[0]) new];
+
+            [composers addObject:[[STGPUImageOutputComposeItem itemWithSourceImage:clipedImage composer: blender] addFilters:@[
+                    [GPUImageTransformFilter transform:CGAffineTransformMakeRotation(AGKDegreesToRadians(degree))]
+            ]]];
+        }
+    }
 
     return [composers reverse];
-}
-
-- (UIImage *__nullable)processImages:(NSArray<UIImage *> *__nullable)sourceImages {
-    return [super processImages:sourceImages];
 }
 
 @end
