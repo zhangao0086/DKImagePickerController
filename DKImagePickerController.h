f39223ca2eeb72e6c00fd6d4c9fb48b40c16fc31
Bannings <zhangao0086@gmail.com>
2018-01-08 17:03:02 +0800

    Add carthage support

---
+++ DKImagePickerController.h
@@ -0,0 +1,19 @@
+//
+//  DKImagePickerController.h
+//  DKImagePickerController
+//
+//  Created by ZhangAo on 08/01/2018.
+//  Copyright © 2018 ZhangAo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKImagePickerController.
+FOUNDATION_EXPORT double DKImagePickerControllerVersionNumber;
+
+//! Project version string for DKImagePickerController.
+FOUNDATION_EXPORT const unsigned char DKImagePickerControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKImagePickerController/PublicHeader.h>
+
+
