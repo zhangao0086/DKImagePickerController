625097dbbe5b8bbfa7a32f3f9f0345f3a29c16fb
hyouuu <hyouuu@gmail.com>
2018-11-05 01:32:36 -0800

    weak self for DKImageBaseManager block

--- Sources/DKImageDataManager/DKImageBaseManager.swift
+++ Sources/DKImageDataManager/DKImageBaseManager.swift
@@ -40,8 +40,9 @@ open class DKImageBaseManager: NSObject, DKImageBaseManagerObserver {
     
     open func notify(with selector: Selector, object: AnyObject?, objectTwo: AnyObject?) {
         if self.observers.count > 0 {
-            let block = {
-                for observer in self.observers.allObjects {
+            let block = { [weak self] in
+                guard let strongSelf = self else { return }
+                for observer in strongSelf.observers.allObjects {
                     if observer.responds(to: selector) {
                         _ = observer.perform(selector, with: object, with: objectTwo)
                     }
